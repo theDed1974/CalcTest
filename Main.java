eaa31051428cbd909662f8d03ae27bcbbb169fae
TheDed1974 <merlynme@vk.com>
2025-01-16 10:35:43 +0700

    restore1

--- Main.java
+++ Main.java
@@ -1,45 +1,47 @@
-import java.util.Objects;
 import java.util.Scanner;  //билиотека со сканером строк
-
 public class Main {
-   public static void main (String[] args) throws Exception {
-       //while(true) {
-           Scanner str = new Scanner(System.in);//получаем строку от пользователя из терминала
-           System.out.println("Введите арифметическое выражение вида: 1 + 2:");
-           String strIn = str.nextLine();
+    public static String calc(String strIn) {
+        String[] strParts;
+        try {
+            // Разделяем строку на части по пробелам
+            strParts = strIn.trim().split("\\s+");
+            if (strParts.length != 3) {
+                throw new Exception("//т.к. строка не является математической операцией");
+            }
 
-           String[] strPath = strIn.split(" ");
-           if (!(strPath.length == 3)) throw new Exception("введите 2 числа и оператор с пробелами между ними");
-           int num1, num2;
-           try {
-               num1 = Integer.parseInt(strPath[0]);
-               num2 = Integer.parseInt(strPath[2]);
-           } catch (Exception e) {
-               throw new Exception("необходимо ввести числа");
-           }
-           if ((num1 < 1 || num1 > 10) || (num2 < 1 || num2 > 10)) throw new Exception("введите числа от 1 до 10");
+            int num1 = Integer.parseInt(strParts[0]);
+            int num2 = Integer.parseInt(strParts[2]);
 
-           if (!(Objects.equals(strPath[1], "+")) && !(Objects.equals(strPath[1], "-")) && !(Objects.equals(strPath[1], "*")) && !(Objects.equals(strPath[1], "/")))
-               throw new Exception();
-           int result = -100;
-           switch (strPath[1]) {
-               case "+":
-                   result = num1 + num2;
-                   break;
-               case "-":
-                   result = num1 - num2;
-                   break;
-               case "*":
-                   result = num1 * num2;
-                   break;
-               case "/":
-                   result = num1 / num2;
-                   break;
-               default:
-                   System.out.println("что то пошло не так...");
-           }
+            // Проверяем, что числа находятся в диапазоне от 1 до 10
+            if (num1 < 1 || num1 > 10 || num2 < 1 || num2 > 10) {
+                throw new Exception("Числа должны быть от 1 до 10");
+            }
 
-           System.out.println(result);
-       //}
+            int result;
+            switch (strParts[1]) {
+                case "+" -> result = num1 + num2;
+                case "-" -> result = num1 - num2;
+                case "*" -> result = num1 * num2;
+                case "/" -> result = num1 / num2;
+                default -> throw new Exception("Неверный оператор. Доступные операторы: +, -, *, /");
+            }
+
+            return String.valueOf(result);
+        } catch (Exception e) {
+            return "Ошибка: " +e.getMessage();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите арифметическое выражение вида: 1 + 2:");
+        String strIn = scanner.nextLine();
+        String result;
+        try {
+            result = calc(strIn);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(result);
     }
 }
